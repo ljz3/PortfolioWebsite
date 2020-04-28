d4f1843e07c02a9c46d14cb27bcc0ba1f1ae84f0
ljz3 <kevin.ottawa@hotmail.com>
2020-04-28 16:46:34 -0400

    Completed title page with typed addon, and superslides

--- js/script.js
+++ js/script.js
@@ -4,4 +4,12 @@ $(document).ready(function(){
         play: 5000,
         pagination: false
     });
+
+    var typed = new Typed(".typed", {
+        strings: ["Student at University of Toronto.", "Computer Science.", "Developer."],
+        typeSpeed: 50,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    })
 });
